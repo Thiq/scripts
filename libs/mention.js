38528b2db7bd308dfee66ad02c75002d3f63d97e
Justin Cox <dubstepandponies95@gmail.com>
2017-12-29 23:11:41 -0700

    Added beginning of message check for mentions

--- libs/mention.js
+++ libs/mention.js
@@ -11,5 +11,14 @@ registerEvent(player, 'chat', function(e) {
             title.subtitle(sentBy + ' mentioned you');
             title.send(player);
         }
+    } else if (/^\@\S\w/i.test(e.getMessage())) {
+        var targets = e.getMessage().split(/\s\@\S\w/i);
+        for (var i = 0; i < targets.length; i++) {
+            var name = targets[i].split(' ')[0];
+            var player = Bukkit.getPlayer(name.substring(1));
+            var sentBy = e.getPlayer().getPlayerListName();
+            title.subtitle(sentBy + ' mentioned you');
+            title.send(player);
+        }
     }
 })
