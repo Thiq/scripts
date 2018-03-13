0d19608ac9a592384cfe27e364fc6ab03cd254a2
Justin Cox <dubstepandponies95@gmail.com>
2018-03-12 21:48:27 -0600

    Some more work on quests. Added TODO to help track myself.

--- libs/helpers.js
+++ libs/helpers.js
@@ -1,14 +1,4 @@
 // Helpers file that can be used across Thiq
-
-Object.prototype.giveItem = function(item) {
-    try {
-        var inventory = this.getInventory();
-        inventory.addItem(item);
-    } catch (e) {
-        throw 'Object has no inventory. Cannot call .giveItem()';
-    }
-};
-
 Object.extend = function(source, target) {
     var properties = Object.getOwnPropertyNames(source);
     for (var i = 0; i < properties.length; i++) {
